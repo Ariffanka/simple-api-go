70cb6b0c6115e05d0124577835643a6ed30d4c87
Ariffanka <aripjah11@gmail.com>
2024-02-25 21:52:13 +0700

    add features

--- controller/bio.controller.go
+++ controller/bio.controller.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+
 )
 
 var db *sql.DB
@@ -39,5 +40,80 @@ func Get(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	json.NewEncoder(w).Encode(bio)
+	data, err := json.MarshalIndent(bio, "", "    ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+func Gets(w http.ResponseWriter, r *http.Request) {
+	var bios []model.Bio
+	rows, err := db.Query("SELECT * FROM bio")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var bio model.Bio
+		if err := rows.Scan(
+			&bio.ID,
+			&bio.Username,
+			&bio.Email,
+		); err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+			return
+		}
+		bios = append(bios, bio)
+	}
+
+	if err := rows.Err(); err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	// Indentasi JSON sebelum menulisnya ke ResponseWriter
+	data, err := json.MarshalIndent(bios, "", "    ")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var bio model.Bio
+	json.NewDecoder(r.Body).Decode(&bio)
+	_, err := db.Exec("UPDATE bio SET username=$1, email=$2 WHERE id=$3", bio.Username, bio.Email, params["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Success update"))
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var bio model.Bio
+	json.NewDecoder(r.Body).Decode(&bio)
+	_, err := db.Exec("DELETE FROM bio WHERE id=$1", params["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Success delete"))
 }
--- routes/route.go
+++ routes/route.go
@@ -11,6 +11,9 @@ func NewRouter() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/users/create", controller.Create).Methods("POST")
 	router.HandleFunc("/users/{id}", controller.Get).Methods("GET")
+	router.HandleFunc("/users/", controller.Gets).Methods("GET")
+	router.HandleFunc("/users/update/{id}", controller.Update).Methods("POST")
+	router.HandleFunc("/users/delete/{id}", controller.Delete).Methods("GET")
 
 	return router
 }
